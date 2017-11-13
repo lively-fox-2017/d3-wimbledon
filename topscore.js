9ce7578412ccb03d76dd8a9646bfe46b214e44cd
redhaputra <putra.redha@gmail.com>
2017-11-13 21:13:44 +0700

    release 5 done

--- topscore.js
+++ topscore.js
@@ -4,45 +4,48 @@ let fill = d3.scaleOrdinal(d3.schemeCategory20)
 let leaderScale = d3.scaleLinear()
   .range([5, 40])
 
+const color = d3.scaleLinear()
+              .domain([0,1,2,3,4,5,6,10,15,20,100])
+              .range(["red", "green", "blue", "cyan", "yellow", "pink", "orange", "purple", "grey", "lime", "gold", "black"]);
+
 const draw = (words) => {
   // Draw your data here...
   // console.log(arr);
 
-  var color = d3.scaleLinear()
-                .domain([0,1,2,3,4,5,6,10,15,20,100])
-                .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
-
-  d3.layout.cloud().size([800, 300])
+  d3.layout.cloud().size([850, 350])
           .words(words)
           .rotate(0)
           .fontSize(function(d) {
             return d.size
           })
-          .on("end", draw)
+          .on("end", setWordsCloud)
           .start()
 
-  d3.select('#top-score').append('svg')
-                        .attr('width', 850)
-                        .attr('height', 350)
-                        .attr('class', 'WordCloud')
-                        .append('g')
-                        .attr('transform', 'translate(320, 200)')
-                        .selectAll('text')
-                        .data(words)
-                        .enter()
-                        .append('text')
-                        .style('font-size', function(d) {
-                          return d.size + "px"
-                        })
-                        .style('fill', function(d, index) {
-                          return color(index)
-                        })
-                        .attr('transform', function(d) {
-                          return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')'
-                        })
-                        .text(function(d) {
-                          return d.text
-                        })
+   function setWordsCloud(setWords) {
+     d3.select('#top-score')
+       .append('svg')
+       .attr('width', 850)
+       .attr('height', 350)
+       .attr('class', 'WordCloud')
+       .append('g')
+       .attr('transform', 'translate(320, 200)')
+       .selectAll('text')
+       .data(setWords)
+       .enter()
+       .append('text')
+       .style('font-size', function(d) {
+         return d.size + "px"
+       })
+       .style('fill', function(d, index) {
+         return color(index)
+       })
+       .attr('transform', function(d) {
+         return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')'
+       })
+       .text(function(d) {
+         return d.text
+       })
+   }
 }
 
 
@@ -54,7 +57,7 @@ const load = () => {
     rows.forEach((row) => {
       obj = {
         text: row.Name,
-        size: row.G
+        size: row.G*2
       }
 
       arr.push(obj)
