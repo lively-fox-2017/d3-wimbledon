65445be0931574a55d9ff301bf3fa916045257df
jasoneffendi <jasoneffendi27@gmail.com>
2017-11-13 15:02:18 +0700

    World cloud and stats done

--- script.js
+++ script.js
@@ -14,13 +14,72 @@ let svg = d3.select('#results')
 
 // Data reloading
 let reload = () => {
+  let data = []  
+  d3.tsv('afcw-results.tsv', (rows) => {
+    console.log(rows)
+    for(var i = 0; i < rows.length; i++) {
+      data.push(rows[i].GoalsScored)
+    }
+
+    redraw(data)
+  })
+
   // Your data parsing here...
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  var linearScale = d3.scaleLinear()
+                    .domain([d3.min(data), d3.max(data)])
+                    .range([0,300])
 
+  var horizontalScale = d3.scaleLinear()
+                    .domain([d3.max(data), d3.min(data)])
+                    .range([0,280])
+
+  var verticalScale = d3.scaleLinear()
+                    .domain([0, data.length])
+                    .range([0,750])
+
+  var bar = svg.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", function(d, i) {
+                return "translate(" + i * 750/data.length + ", 0)";
+            });
+  
+  bar.append("rect")
+      .attr("y", function(d) { return 280 - linearScale(d); })
+      .attr("height", function(d) {
+          return linearScale(d);
+      })
+      .attr("width", function(d, i) {
+          return 13
+      });
+
+  var x_axis = d3.axisBottom()
+              .scale(verticalScale);
+
+  var y_axis = d3.axisLeft()
+              .scale(horizontalScale);
+  
+  svg.append("g")
+     .attr("transform", "translate(30, 0)")
+     .call(y_axis);
+
+  svg.append("g")
+      .attr("transform", "translate(0, 280)")
+      .call(x_axis)
+
+
+  var t = d3.transition()
+          .duration(750)
+          .ease(d3.easeLinear);
+  
+  d3.selectAll("g").transition(t).style("fill","blue");
+           
 }
 
 reload()
--- topscore.js
+++ topscore.js
@@ -6,12 +6,54 @@ let leaderScale = d3.scaleLinear()
 
 const draw = (words) => {
   // Draw your data here...
+  var color = d3.scaleLinear()
+  .domain([0,1,2,3,4,5,6,10,15,18,20])
+  .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
 
+  d3.layout.cloud().size([800, 300])
+    .words(words)
+    .rotate(0)
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
+
+  function draw(words) {
+  d3.select("#top-score").append("svg")
+        .attr("width", 1200)
+        .attr("height", 1200)
+        .attr("class", "wordcloud")
+        .append("g")
+        .attr("transform", "translate(320,200)")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("fill", function(d, i) { return color(i); })
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
 }
 
 
 const load = () => {
   // Load your data here...
+  d3.tsv('stats.tsv', (rows) => {
+    console.log(rows)
+    let data = []
+    for(var i = 0; i < rows.length; i++) {
+      let obj = {
+        text: rows[i].Name,
+        size: rows[i].G*5
+      }
+      data.push(obj)
+    }
+    data.sort(function(a, b) {
+      return a.score - b.score;
+    });
+    draw(data.reverse())
+  })
 }
 
 load()
