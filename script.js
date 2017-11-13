aec0bab85e3c195a5be2cc4cce8545babdbc2543
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-13 18:55:56 +0700

    goals and topscore visualization

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // Our canvas
 const width = 750,
   height = 300,
-  margin = 20
-marginLeft = 40
+  margin = 20,
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,12 +15,53 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', rows => {
+    const goalsScoredData = rows.map(row => row.GoalsScored)
+    redraw(goalsScoredData)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  const yScale = 
+  d3.scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([height - 50, 0])
 
+  const xScale =
+  d3.scaleLinear()
+  .domain([0, data.length])
+  .range([50, width - 50])
+
+  const bottomAxis = 
+  d3.axisBottom(xScale)
+  .ticks(data.length)
+
+  const leftAxis = 
+  d3.axisLeft(yScale)
+  .ticks(d3.max(data))
+
+  svg
+  .selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('transform', 'translate(0, -25)')
+  .attr('x', (d, idx) => xScale(idx))
+  .attr('y', d => 300 - (yScale(0) - yScale(d)))
+  .attr('width', 12)
+  .attr('height', d => yScale(0) - yScale(d))
+  .attr('fill', 'teal')
+
+  svg.append('g')
+  .attr('transform', 'translate(0, 275)')
+  .call(bottomAxis)
+
+  svg.append('g')
+  .attr('height', 200)
+  .attr('transform', 'translate(50, 25)')
+  .call(leftAxis)
 }
 
 reload()
--- topscore.js
+++ topscore.js
@@ -6,12 +6,38 @@ let leaderScale = d3.scaleLinear()
 
 const draw = (words) => {
   // Draw your data here...
+  d3.select('#top-score')
+  .append('svg')
+  .attr('width', 1700)
+  .attr('height', 700)
+  .attr("class", "wordcloud")
+  .append('g')
+  .attr('transform', 'translate(640, 200) scale(1.5)')
+  .selectAll('text')
+  .data(words)
+  .enter()
+  .append('text')
+  .style('font-size', d => d.goals + 'px')
+  .style('fill', (d, i) => fill(i))
+  .attr('text-anchor', 'middle')
+  .attr('transform', d => 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ') scale(1.5)')
+  .text(d => d.name)
 
 }
 
-
 const load = () => {
   // Load your data here...
+  d3.tsv('stats.tsv', rows => {
+    let words = rows.map(row => ({name: row.Name, goals: row.G})).sort((a, b) => b.goals - a.goals)
+
+    d3.layout.cloud()
+    .size([1600, 600])
+    .words(words)
+    .rotate(0)
+    .fontSize(d => d.goals)
+    .on('end', draw)
+    .start();
+  })
 }
 
 load()
