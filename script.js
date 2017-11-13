6615b4f19810408da344b23a06fca7c2cdb1fb88
kholes <cholis.elbazar@gmail.com>
2017-11-13 19:18:01 +0700

    graph with axis

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const width = 750,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,12 +15,39 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', (rows) => {
+    let dataMap = rows.map(row => {
+      return row.GoalsScored
+    })
+    redraw(dataMap)    
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
-
+  const yScale = d3.scaleLinear()
+                   .domain([0, d3.max(data)])
+                   .range([0, height])
+  const xScale = d3.scaleLinear()
+                   .domain([0, data.length])
+                   .range([0, width])
+  let rect = svg.selectAll('rect')
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr('class', 'bar')
+                .attr('x', (d, i) => { return 25 * (i + 1.2) })
+                .attr('y', (d) => { return (height - 20) - yScale(d)})
+                .attr('width', 20 + 'px')
+                .attr('height', (d) => { return yScale(d) })
+                let yAxis = d3.axisLeft().scale(yScale)
+                let xAxis = d3.axisBottom().scale(xScale)            
+                svg.append('g')
+                .attr('transform', 'translate(30, 5)')
+                .call(yAxis)
+                svg.append('g')
+                .attr('transform', 'translate(5, 280)')
+                .call(xAxis)
 }
-
 reload()
