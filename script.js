e55da6c374877c99117733457437bd54bb0397a3
dimasgardenia <dimas.gardenia@gmail.com>
2017-11-13 15:56:05 +0700

    add refactor code

--- script.js
+++ script.js
@@ -15,29 +15,45 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  let state = []
   d3.tsv('afcw-results.tsv', (rows) => {
   console.log(rows)
-    svg
-    .selectAll('rect')
-    .data(rows)
-    .enter()
-    .append('rect')
-    .attr('width', 20)
-    .attr('height', 201)
-    .attr('fill', '#0000')
-    .attr('x', (d, i) => {
-      return i * 25
-    })
-    .attr('y', (d, i) => {
-      return i
-    })
+    state = rows
+    redraw(state)
   })
 }
 
 // redraw function
-let redraw = (data) => {
+let redraw = (state) => {
   // Your data to graph here
+  let dataGoals = state.map((data) => {
+    return data.GoalsScored
+  })
+  console.log('ini state data goals', dataGoals)
 
+const yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataGoals)])
+    .range([0, height])
+
+const xScale = d3.scaleLinear()
+    .range([0, width])
+
+    svg.selectAll('rect')
+    .data(dataGoals)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => {
+      return i * 25
+    })
+    .attr('y', (d) => {
+      return height - yScale(d)
+    })
+    .attr('width', 20)
+    .attr('height', (d) => {
+      return yScale(d)
+    })
+    .attr('fill', yScale)
 }
 
 reload()
