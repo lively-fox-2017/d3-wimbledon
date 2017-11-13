649a24d562350f07cb5fc97fa3a6152a1164daa9
Dimitri Wahyudiputra <deathmitri@gmail.com>
2017-11-13 20:51:14 +0700

    Fixed xScale

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* global d3 */
 
 // Our canvas
-const canvasWidth = 750,
-      canvasHeight = 300,
-      canvasMargin = 20
+const canvasMargin = 20
       canvasMarginLeft = 40
+      canvasWidth = 750,
+      canvasHeight = 300,
 
 // Drawing area
 d3.select('#results')
@@ -17,39 +17,42 @@ let svg = d3.select('svg')
 // Data reloading
 let reload = () => {
   d3.tsv('afcw-results.tsv', (games) => {
-
-    const goals = [];
-
-    games.forEach((game) => {
-      goals.push(parseInt(game.GoalsScored))
-    })
-
-    const yScale = d3.scaleLinear()
-                     .domain([0, Math.max(...goals)])
-                     .range([0, canvasHeight])
-
-    svg.selectAll('rect')
-       .data(games)
-       .enter()
-       .append('rect')
-       .attr('width', canvasMargin)
-       .attr('height', (game) => {
-          return game.GoalsScored * 60
-       })
-       .attr('x', (game, index) => {
-          return index * 22
-       })
-       .attr('y', (game) => {
-          return canvasHeight - game.GoalsScored * 60
-       })
-       .attr('fill', 'teal')
+    redraw(games)
   })
 }
 
 // redraw function
-let redraw = (data) => {
+let redraw = (games) => {
   // Your data to graph here
+  const goals = [];
 
+  games.forEach((game) => {
+    goals.push(parseInt(game.GoalsScored))
+  })
+
+  const xScale = d3.scaleLinear()
+                   .domain([0, games.length])
+                   .range([0, canvasWidth - canvasMargin])
+
+  const yScale = d3.scaleLinear()
+                   .domain([0, Math.max(...goals)])
+                   .range([canvasHeight - canvasMargin, 0])
+
+  svg.selectAll('rect')
+     .data(games)
+     .enter()
+     .append('rect')
+     .attr('width', canvasMargin / 2)
+     .attr('height', (game) => {
+       return yScale(0) - yScale(game.GoalsScored)
+     })
+     .attr('x', (game, index) => {
+       return xScale(index)
+     })
+     .attr('y', (game) => {
+       return yScale(game.GoalsScored)
+     })
+     .attr('fill', 'teal')
 }
 
 reload()
